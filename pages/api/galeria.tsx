9e2385ac63d071bd79a9f721f98b98121d130aae
Guilherme S <guinho.shuto@gmail.com>
2022-09-02 19:45:10 -0400

    galeria api

---
+++ pages/api/galeria.tsx
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+// import Contentful from "../services/contentful";
+import * as contentful from "contentful";
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+){
+    
+    const galeria = await client.getEntries('6wctVRNvKO4hZPBRmcCWdH')
+
+    res.status(200).json({galeria: galeria})
+}
