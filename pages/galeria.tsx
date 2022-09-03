ec501c94c6eab7812c6a5fd014617657dcde3bc6
Guilherme S <guinho.shuto@gmail.com>
2022-09-02 20:27:07 -0400

    galeria

--- pages/galeria.tsx
+++ pages/galeria.tsx
@@ -1,13 +1,32 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import axios from 'axios'
 import Header from './components/Header'
 
-const Galeria: NextPage = () => {
+const Galeria: NextPage = ({galeria}: any) => {
     return(
         <div>
             <Header />
-            ainda não fiz
+            <div>
+                {galeria.includes.Asset.map((images: any, index: number) => (
+                    <div key={index}><Image src={images.fields.file.url} alt="galeria" /></div>
+                ))}
+            </div>
         </div>
     )
 }
 
 export default Galeria
+
+export async function getServerSideProps(){
+  const res = await axios.get("https://culto.agropescajacare.com.br/api/galeria");
+  const arr = JSON.parse(JSON.stringify(res.data.galeria));
+  arr.reverse();
+
+  return{
+    props: {
+        galeria: arr
+    }
+  }
+
+}
