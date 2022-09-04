52d9df60857eb8be04311b9dcf760bb719069db6
Guilherme S <guinho.shuto@gmail.com>
2022-09-03 22:08:34 -0400

    clip aqualua

--- pages/api/twitch.tsx
+++ pages/api/twitch.tsx
@@ -1,11 +1,22 @@
+import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const header = {
-    
+const headers = {
+  headers: {
+    "Authorization": process.env.TWITCH_TOKEN!, 
+    "Client-Id": process.env.TWITCH_CLIENT_ID!
+  }
 }
 
 export default async function handler(
     req: NextApiRequest, res: NextApiResponse
 ){
+    const url = "https://api.twitch.tv/helix/videos?user_id=546486945&limit=1000"
+    axios.get(url, headers)
+    .then(data => {
+        // const videos = data.data.data.filter(video => video.)
+        console.log(data.data.data)
+    })
+
     res.status(200).json({})
 }
--- pages/clips.tsx
+++ pages/clips.tsx
@@ -1,11 +1,27 @@
-import type { NextPage } from 'next'
-import Header from './components/Header'
+import type { NextPage } from 'next';
+import styles from '../styles/Clips.module.css';
+import Header from './components/Header';
+
+const videos = [
+    ''
+]
 
 const Clips: NextPage = () => {
     return(
         <div>
             <Header />
-            oi
+            <h1>CLIPS</h1>
+            <div className={styles.container}>
+                <iframe
+                    title="preview"
+                    src={`https://player.twitch.tv/?collection=ex8rmLoFBhd32Q&parent=localhost&parent=agropescajacare.com.br`}
+                    // height="100%"
+                    // width="50%"
+                    height="720"
+                    width="1280"
+                    allowFullScreen>
+                </iframe>
+            </div>
         </div>
     )
 }
