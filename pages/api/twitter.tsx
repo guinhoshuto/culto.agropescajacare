9627747e23bc444c8e4aeb465d01042316400abe
Guilherme S <guinho.shuto@gmail.com>
2022-09-04 03:40:54 -0400

    ajuste na response do twitter

--- pages/api/twitter.tsx
+++ pages/api/twitter.tsx
@@ -13,9 +13,9 @@ export default async function handler(
     const url = "https://api.twitter.com/2/tweets/search/recent?query=conversation_id:1565054846956445697&tweet.fields=in_reply_to_user_id,author_id,created_at,conversation_id&max_results=100&sort_order=recency"
     axios.get(url, headers)
     .then(data => {
-        // const videosdata.data.data.filter
+        const thread = data.data.data.filter((t:any ) => t.author_id === '3038290799')
         console.log(data.data.data)
-        res.status(200).json({})
+        res.status(200).json({thread: thread})
     })
 
 }
--- pages/lore.tsx
+++ pages/lore.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Header from './components/Header'
+import axios from 'axios'
 
 const Lore: NextPage = () => {
     return(
@@ -11,3 +12,14 @@ const Lore: NextPage = () => {
 }
 
 export default Lore
+
+export async function getServerSideProps(){
+    const res = await axios.get("https://culto.agropescajacare.com.br/api/twitter");
+    const arr = JSON.parse(JSON.stringify(res.data.galeria));
+
+    return{
+        props: {
+            thread: arr
+        }
+    }
+}
