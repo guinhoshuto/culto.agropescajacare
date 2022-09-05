2ffcb70e93a9bf730b528b44b222b07a567a68b3
Guilherme S <guinho.shuto@gmail.com>
2022-09-04 21:32:47 -0400

    push pra vercel

--- pages/components/Tweet.tsx
+++ pages/components/Tweet.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/Tweet.module.css'
 const Tweet = ({tweet}: any) => {
     return(
         <div className={styles.tweet}>
-            {tweet.text}
+            {/* {tweet.text} */}
         </div>
     )
 }
