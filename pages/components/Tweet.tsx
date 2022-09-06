da6c19e6dfce9f90317b0368d66bf31e7c598666
Guilherme S <guinho.shuto@gmail.com>
2022-09-06 17:07:09 -0400

    verificacao no dumb component

--- pages/components/Tweet.tsx
+++ pages/components/Tweet.tsx
@@ -3,9 +3,7 @@ import styles from '../../styles/Tweet.module.css'
 const Tweet = ({tweet}: any) => {
     return(
         <div className={styles.tweet}>
-            {console.log(tweet)}
-            {console.log('oi')}
-            {tweet.text}
+            {tweet && tweet.text}
         </div>
     )
 }
