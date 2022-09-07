3de7bb953fe0de06b4fadc96406d954a7d80bb76
Guilherme S <guinho.shuto@gmail.com>
2022-09-07 19:21:17 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </div>
+  ) 
 }
 
 export default MyApp
