0b76625fc645a5e7d79593836fcd1408191570ed
Guilherme S <guinho.shuto@gmail.com>
2022-09-07 17:26:49 -0400

    css de imagem e ajustes no responsivo

--- pages/components/Tweet.tsx
+++ pages/components/Tweet.tsx
@@ -1,11 +1,17 @@
 import styles from '../../styles/Tweet.module.css'
+import Image from 'next/image'
 
 const Tweet = ({tweet}: any) => {
     return(
         <a href={`https://twitter.com/guzcalp/status/${tweet ? tweet.id : ''}`}>
-            <div className={styles.tweet}>
-                {tweet && tweet.text}
-            </div>
+            {tweet && 
+                <div className={styles.tweet}>
+                    <div className={styles.tweetText}>
+                        {tweet.text} 
+                    </div>
+                    {tweet.media_url && <div className={styles.tweetImage}><Image src={tweet.media_url} alt="" width={300} height={200} layout="responsive"/></div>}
+                </div>
+            }
         </a>
     )
 }
